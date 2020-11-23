5e2b1bf3ace3b2b54ceb2f7aefa4622301953665
Erik Nicolao <erik.nicolao@studenti.unitn.it>
2020-11-23 17:23:09 +0100

    Aggiunto missions API

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,13 @@
 const news = require('./news.js');
 const astronauts = require('./astronauts.js');
+const missions = require('./missions.js');
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.use('/news', news);
-
+app.use('/missions', missions)
 app.use('/astronauts', astronauts);
 
 app.listen(port, function() {
