1de5d98abfb9f49c9be7a468a59ea0e87eb4ddc5
Lorenzo Gnech <lorenzognech@gmail.com>
2020-11-23 15:47:09 +0100

    Added the frontend starting point

---
+++ Frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id="nav">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    components: {
+    },
+    data: function(){
+        return{
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: "Playfair Display", "Roboto";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 0;
+
+  a {
+    font-weight: bold;
+    color:whitesmoke;
+
+    &.router-link-exact-active {
+      color: lightseagreen;
+    }
+  }
+
+  a:hover {
+    background-color: #111;
+  }
+}
+
+</style>
