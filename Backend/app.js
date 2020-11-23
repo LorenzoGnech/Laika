b3645b14b1c18e5191e691a6718cd1095bef1c36
Martiriak <dominiquepegoraro@gmail.com>
2020-11-23 17:18:57 +0100

    reso figo app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
+var path = require('path');
+var serveStatic = require('serve-static');
 
+var app = express();
 var port = process.env.PORT || 3000;
 
+console.log(__dirname);
+
+app.use(serveStatic(__dirname + "/Frontend/dist"));
+
 // Handling GET requests
-app.get('/', function(req, res){ 
+app.get('/ciao', function(req, res){ 
   res.send('Hello World!');
 });
 
