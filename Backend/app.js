3b573eaaaaad9a0297149c7497f47a9b439e4b24
layneeeee <lorenzognech@gmail.com>
2020-11-23 15:15:15 +0100

    Added .gitignore and started express application

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+// Handling GET requests
+app.get('/', function(req, res){ 
+  res.send('Hello World!');
+});
+
+app.listen(port, function() {
+  console.log('Server running on port ', port);
+});
