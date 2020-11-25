098dd7678a15bc5fbaa1af7db1bd87a98e2f18d0
LorenzoGnech <lorenzognech@gmail.com>
2020-11-25 11:22:04 +0100

    Removed CORS policies that interferes with our APIs

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,14 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
