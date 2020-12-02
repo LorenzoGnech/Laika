4ccb75da733260d343b4f415f706b31d076917eb
R0gia <taufersimo@gmail.com>
2020-12-02 16:16:44 +0100

    updated missions.js and exoplanets.js

--- mongodb/exoplanets.js
+++ mongodb/exoplanets.js
@@ -1,3 +1,12 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema;
+
+module.exports = mongoose.model('exoplanets', new Schema({
+    name: String,
+    description: String,
+    discover_date: Date,
+    img_path: String,
+    source_url: String,
+    tags: [String]
+}));
--- mongodb/missions.js
+++ mongodb/missions.js
@@ -1,3 +1,12 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema;
+
+module.exports = mongoose.model('missions', new Schema({
+    date: Date,
+    title: String,
+    description: String,
+    img_path: String,
+    source_url: String,
+    tags: [Array]
+}));
