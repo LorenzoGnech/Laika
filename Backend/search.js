8c9c4a61d2c17ab2d943a5bde9f6395c2e08cbeb
R0gia <taufersimo@gmail.com>
2020-12-14 15:21:30 +0100

    Prova di API per la ricerca

--- Backend/search.js
+++ Backend/search.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const {dbErrorHandler} = require('./utilities');
+const mongoose = require('mongoose');
+const Missions = require("./models/missions");
 const router = express.Router();
 
 router.get('/:query', async (req, res) => {
+    
     var query = req.params.query
     query = query.split("+")
+
+    Missions.find({tags:{$in: query}})   
+    .exec()
+
+    .then(doc => {
+        console.log(doc);
+        res.status(201).json(doc);
+    })
+
+    .catch(err => {
+        let {errorCode, errorMsg} = dbErrorHandler(err);
+    
+        res.status(errorCode).json({
+            error: errorMsg
+        });
+    });
     
     res.status(200).send(query);
 });
