e01a4b0b5fc9c16be4b9369cea0596dc7c4ab041
R0gia <taufersimo@gmail.com>
2020-12-14 16:12:43 +0100

    Aggiunti vari API per la ricerca

--- Backend/search.js
+++ Backend/search.js
@@ -2,14 +2,84 @@ const express = require('express');
 const {dbErrorHandler} = require('./utilities');
 const mongoose = require('mongoose');
 const Missions = require("./models/missions");
+const News = require("./models/news");
+const Telescopes = require("./models/telescopes");
+const Exoplanets = require("./models/exoplanets");
 const router = express.Router();
 
-router.get('/:query', async (req, res) => {
+router.get('/missions/:query', async (req, res) => {
     
     var query = req.params.query
     query = query.split("+")
 
     Missions.find({$or:[{tags:{$in:query}},{title:{$in:query}}]})   
+    .exec()
+
+    .then(doc => {
+        console.log(doc);
+        res.status(201).json(doc);
+    })
+
+    .catch(err => {
+        let {errorCode, errorMsg} = dbErrorHandler(err);
+    
+        res.status(errorCode).json({
+            error: errorMsg
+        });
+    });
+});
+
+
+router.get('/news/:query', async (req, res) => {
+    
+    var query = req.params.query
+    query = query.split("+")
+
+    News.find({$or:[{tags:{$in:query}},{title:{$in:query}}]})   
+    .exec()
+
+    .then(doc => {
+        console.log(doc);
+        res.status(201).json(doc);
+    })
+
+    .catch(err => {
+        let {errorCode, errorMsg} = dbErrorHandler(err);
+    
+        res.status(errorCode).json({
+            error: errorMsg
+        });
+    });
+});
+
+router.get('/telescopes/:query', async (req, res) => {
+    
+    var query = req.params.query
+    query = query.split("+")
+
+    Telescopes.find({$or:[{tags:{$in:query}},{name:{$in:query}}]})   
+    .exec()
+
+    .then(doc => {
+        console.log(doc);
+        res.status(201).json(doc);
+    })
+
+    .catch(err => {
+        let {errorCode, errorMsg} = dbErrorHandler(err);
+    
+        res.status(errorCode).json({
+            error: errorMsg
+        });
+    });
+});
+
+router.get('/exoplanets/:query', async (req, res) => {
+    
+    var query = req.params.query
+    query = query.split("+")
+
+    Exoplanets.find({$or:[{tags:{$in:query}},{name:{$in:query}}]})   
     .exec()
 
     .then(doc => {
