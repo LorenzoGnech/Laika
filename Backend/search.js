c36f883db2129b8d8ff2e44273f47aa096370aa1
Jefry99 <maic1305@gmail.com>
2020-12-14 14:33:54 +0100

    inizial commit

--- Backend/search.js
+++ Backend/search.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get('/:query', async (req, res) => {
     var query = req.params.query
     query = query.split("+")
+    
     res.status(200).send(query);
 });
 
