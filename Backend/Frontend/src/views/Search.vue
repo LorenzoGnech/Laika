31342a1e88029c11433d8e862a94ff30fe5c4cea
Jefry99 <maic1305@gmail.com>
2020-12-14 14:32:33 +0100

    inizializzata pagina di ricerca

--- Backend/Frontend/src/views/Search.vue
+++ Backend/Frontend/src/views/Search.vue
@@ -4,23 +4,64 @@
               <NavMenu/>
           </div>
     <div class="container">
-          <h1 style="color: white">Work in progress...</h1>
+      <div class="container containertitle-link" id="latestnews" ref="latestnews">
+        <h2 class="containertitle" @click="handleClick('LatestNews')">NEWS</h2>
+        <div v-if="news">
+          <CardGrid :cards=computedNews :cardsHeight="getCardsHeight" :cardsWidth="getCardsWidth" type="news"/>
+        </div>
+        <div v-else>
+          <h2 class="containertitle" @click="handleClick('LatestNews')">NOTHING TO SHOW</h2>
+        </div>
+      </div>
+      <div class="container containertitle-link" id="latestmissions" ref="latestmissions">
+        <h2 class="containertitle" @click="handleClick('missions')">MISSIONS</h2>
+        <div v-if="missions">
+          <CardGrid :cards=computedMissions :cardsHeight="getCardsHeight" :cardsWidth="getCardsWidth" type="mission"/>
+        </div>
+        <div v-else>
+          <h2 class="containertitle" @click="handleClick('LatestNews')">NOTHING TO SHOW</h2>
+        </div>
+      </div>
+      <div class="container containertitle-link" id="latesttelescopes" ref="latesttelescopes">
+        <h2 class="containertitle" @click="handleClick('telescopes')">TELESCOPES</h2>
+        <div v-if="telescopes">
+          <CardGrid :cards=computedTelescopes :cardsHeight="getCardsHeight" :cardsWidth="getCardsWidth" type="telescope"/>
+        </div>
+        <div v-else>
+          <h2 class="containertitle" @click="handleClick('LatestNews')">NOTHING TO SHOW</h2>
+        </div>
+      </div>
+      <div class="container containertitle-link" id="latestexoplanets" ref="latestexoplanets">
+        <h2 class="containertitle" @click="handleClick('exoplanets')">EXOPLANETS</h2>
+        <div v-if="exoplanets">
+          <CardGrid :cards=computedExoplanets :cardsHeight="getCardsHeight" :cardsWidth="getCardsWidth" type="exoplanet"/>
+        </div>
+        <div v-else>
+          <h2 class="containertitle" @click="handleClick('LatestNews')">NOTHING TO SHOW</h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NavMenu from '../components/NavMenu.vue'
+import CardGrid from '../components/CardGrid.vue'
 import axios from 'axios'
 
 export default {
   name: 'Search',
   components: {
+    CardGrid,
     NavMenu
   },
   data: function(){
     return {
       result: [],
+      news: false,
+      missions: false,
+      telescopes: false,
+      exoplanets: false,
     }
   },
   mounted(){
@@ -39,4 +80,21 @@ export default {
 
 <style lang="scss" scoped>
 @import url(//db.onlinewebfonts.com/c/00370d92f1b404768d34ac7dc664289e?family=Sansation);
+
+.containertitle{
+  letter-spacing: 3px;
+  color: whitesmoke;
+  font-size: 30px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  width: 100%;
+}
+
+.container {
+  margin-top: 4vh;
+  padding: 40px 80px;
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
