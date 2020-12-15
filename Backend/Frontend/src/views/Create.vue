d27ac1a05604393613245b2ebccf9149b9216e64
Jefry99 <maic1305@gmail.com>
2020-12-15 11:11:56 +0100

    introdotto metodo put

--- Backend/Frontend/src/views/Create.vue
+++ Backend/Frontend/src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-          <div class="sticky" v-sticky="true" sticky-offset="{top: 0, right: 0}" sticky-side="both" on-stick="onStick" sticky-z-index="20">
+  <div id="app" v-if="this.verificato">
+    <div class="sticky" v-sticky="true" sticky-offset="{top: 0, right: 0}" sticky-side="both" on-stick="onStick" sticky-z-index="20">
               <NavMenu/>
           </div>
           <form id="post" @submit.prevent="submit">
@@ -20,12 +20,18 @@
               <option value="delete">Delete</option>
             </select>
             <br>
-              <textarea id="textarea" name="textarea" rows="15" cols="100">Write as json!</textarea>
+              <textarea :value="this.placeholder" id="textarea" name="textarea" rows="15" cols="100"></textarea>
             <br>
-              <textarea id="id" name="id" rows="1" cols="50">Put your ID here, if needed!</textarea>
+              <textarea id="id" name="id" rows="1" cols="60">Put the ID for PUT or DELETE! (you can find id in the url)</textarea>
             <br>
             <input type="submit" value="Submit">
           </form>
+  </div>
+  <div id="app" v-else>
+    <div class="sticky" v-sticky="true" sticky-offset="{top: 0, right: 0}" sticky-side="both" on-stick="onStick" sticky-z-index="20">
+              <NavMenu/>
+          </div>
+    <h1 id="scritta">YOU DON'T HAVE THE RIGHT PRIVILEGES</h1>
   </div>
 </template>
 
@@ -39,7 +45,53 @@ export default {
   components: {
     NavMenu
   },
+  mounted() {
+    this.create_string();
+    this.isVerificato();
+  },
   methods: {
+    isVerificato(){
+      if (this.$store.getters.getUsername == "admin@admin.admin"){
+        this.verificato = true;
+      }
+    },
+    create_string(){
+      var a = {"date":"23-234-234",
+              "title":"32874t",
+              "content":"kjnbasdkfj",
+              "img_path":"sdf",
+              "source_url":"wdfwd",
+              "tags": ["sdf","sdf"]}
+      this.placeholder = "NEWS:\n" + JSON.stringify(a);
+      var a = {"date":"23-234-234",
+              "title":"32874t",
+              "description":"kjnbasdkfj",
+              "img_path":"sdf",
+              "source_url":"wdfwd",
+              "tags": ["sAAdf","sFFdf"]}
+      this.placeholder += "\nMISSIONS:\n" + JSON.stringify(a);
+      var a = {"birth":"2020-2-10",
+              "name":"SHREK",
+              "nationality":"PALUDE",
+              "img_path":["sdf"],
+              "agency":"ciuchino",
+              "tags": ["sAAdf","sFFdf"]}
+      this.placeholder += "\nASTRONAUTS:\n" + JSON.stringify(a);
+      var a = {"discover_date":"2020-2-10",
+              "name":"SHREK",
+              "description":"PALUDE",
+              "img_path":"sdf",
+              "source_url":"ciuchino",
+              "tags": ["sAAdf","sFFdf"]}
+      this.placeholder += "\nEXOPLANETS:\n" + JSON.stringify(a);
+      var a = {"launch_date":"2020-2-10",
+              "name":"SHREK",
+              "description":"PALUDE",
+              "img_path":["sdf"],
+              "source_url":"ciuchino",
+              "tags": ["sAAdf","sFFdf"]}
+      this.placeholder += "\nTELESCOPES:\n" + JSON.stringify(a);
+    },
     parser(tipo, obj){
       let params = new URLSearchParams();
       params.append('token', this.$store.getters.getToken);
@@ -85,17 +137,23 @@ export default {
           params.append('tags', obj.tags);
           break;
       }
-      console.log(params.toString());
       return params;
     },
     delete(richiesta){
-      console.log("delete")
+      let params = new URLSearchParams();
+      params.append('token', this.$store.getters.getToken);
+      let url = "https://laikapp.herokuapp.com/api/v1/" + richiesta.tipo  + "/" + richiesta.id;
+      axios.delete(url)
+        .catch(err => console.warn(err));;
     },
     put(richiesta){
-      console.log("put")
+      let obj = JSON.parse(richiesta.text);
+      var params = this.parser(richiesta.tipo, obj)
+      console.log('https://laikapp.herokuapp.com/api/v1/' + richiesta.tipo  + "/" + richiesta.id);
+      axios.put('https://laikapp.herokuapp.com/api/v1/' + richiesta.tipo  + "/" + richiesta.id, params)
+        .catch(err => console.warn(err));;
     },
     post(richiesta){
-      console.log("post")
       let obj = JSON.parse(richiesta.text);
       var params = this.parser(richiesta.tipo, obj)
       axios.post('http://localhost:3000/api/v1/' + richiesta.tipo, params)
@@ -118,6 +176,8 @@ export default {
   data: function(){
     return {
       loginTitle: 'Login',
+      placeholder: "",
+      verificato: false,
     }
   },
 }
@@ -138,6 +198,10 @@ export default {
 
 #method{
   margin: 10px;
+}
+
+#scritta{
+  padding-top: 30px;
 }
 
 </style>
